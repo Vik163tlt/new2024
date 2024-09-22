f115cab5fdbff2da9fc6ed28d4e33544b6428d82
Vik163tlt <@Vik163tlt@ya.ru>
2024-09-22 21:05:29 +0300

    commit2

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,23 @@
+import javax.swing.*;
 import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
 
-    public static int[] firstArray = new int []{0,1,2,3,4,5,6,7,8,9};
-    public static int[] secondArray = new int []{10,11,12,13,14,15,16,17,18,19};
-    public static int[] resultArray;
-
 
     public static void main(String[] args) {
-resultArray = new int[firstArray.length + secondArray.length];
-        System.arraycopy(firstArray, 0, resultArray, 0, firstArray.length);
-        System.arraycopy(secondArray, 0, resultArray, firstArray.length, secondArray.length);
-        for (int j : resultArray) {
-            System.out.print(j + ", ");
-        }
+        long a =2;
 
+        System.out.println(nightDegree(a));
     }
+public static long nightDegree(long a){
+        return cube(cube(a));
+}
+public static long cube(long a){
+        return a*a*a;
+}
 }
 
 
